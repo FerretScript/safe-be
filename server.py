1138f75120ed116d0c221b7f7448ac6e7c32000e
DanielBrMz <a01254805@tec.mx>
2024-09-09 12:00:00 -0600

    :/

--- server.py
+++ server.py
@@ -16,7 +16,6 @@ os.environ["MYSCALE_HOST"] = "msc-5131d230.us-east-1.aws.myscale.com"
 os.environ["MYSCALE_PORT"] = "443"
 os.environ["MYSCALE_USERNAME"] = "danielbrmz_org_default"
 os.environ["MYSCALE_PASSWORD"] = "passwd_ktandddcNhsCy4"
-os.environ["ANTHROPIC_API_KEY"] = "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"
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'csv', 'pdf'}
